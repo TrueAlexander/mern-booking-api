163d09725ef3f4b1d82bea0127107a8d6c06850e
Alexander <caminante.msk@gmail.com>
2022-10-24 19:05:33 -0300

    adding to front

--- routes/hotels.js
+++ routes/hotels.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js"
+import { countByCity, countByType, createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js"
 import { verifyAdmin } from "../utils/verifyToken.js"
 
 const router = express.Router()
@@ -10,8 +10,11 @@ router.put("/:id", verifyAdmin, updateHotel)
 //delete
 router.delete("/:id", verifyAdmin, deleteHotel)
 //get
-router.get("/:id", getHotel)
+router.get("/find/:id", getHotel)
 //get all
 router.get("/", getHotels)
+
+router.get("/countByCity", countByCity)
+router.get("/countByType", countByType)
 
 export default router
